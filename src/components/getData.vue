e4fd00504ffa362aae83708dce921d01d10cba1d
T480Mac <942385056@qq.com>
2022-01-06 17:00:37 +0800

    增加翻页搜索功能

--- src/components/getData.vue
+++ src/components/getData.vue
@@ -9,7 +9,7 @@
             v-model="title"
             placeholder="输入想搜索的主题"
             class="input-with-select"
-            @keyup.enter.prevent="searchEnterFun($event)"
+            @keyup.enter.prevent="searchEnterFun"
         >
           <template #append>
             <el-button @click="getForm">Go</el-button>
@@ -28,6 +28,7 @@
         </template>
         </el-table-column>
       </el-table>
+      <el-pagination layout="total, prev, pager, next, jumper" :total="60" :page-size="10" @current-change="handleCurrentChange"></el-pagination>
     </el-col>
     <el-col :offset="1" :span="7">
       <iframe :src=this.drawerUrl style="width: 100%;height: 100%"></iframe>
@@ -44,21 +45,22 @@ export default {
   name: "getData",
   data() {
     return {
-      url_favorite:'/api?m=App&c=Article&a=getHotMainArticle&uid=1638493790&page=1&pagesize=50',
+      url_favorite:'/api?m=App&c=Article&a=getHotMainArticle&uid=1638493790&page=1&pagesize=10',
       url_java:'/api?m=App&c=index&a=getCategoryArticle&uid=1638493790&fid=8&page=1&pagesize=10',
-      url_custom:'/api?m=App&c=Article&a=searchArticle&uid=1638493790&page=1&pagesize=50&title=',
+      url_custom:'/api?m=App&c=Article&a=searchArticle&uid=1638493790&pagesize=20&title=',
       drawer:'',
       title:'',
       drawerUrl:'http://www.qidianlife.com/Singular/index.php?m=App&c=Article&a=index&id=7267',
-      tableData:[]
+      tableData:[],
+      currentPage:1
     }
   },
   created() {
-    this.getForm()
+    this.getForm('1')
   },
   methods:{
-    getForm() {
-      axios.get(this.url_custom+this.title)
+    getForm(pageno) {
+      axios.get(this.url_custom+this.title+'&page='+pageno)
           .then(res=> {
             let tempdata = res.data.data;
             //  方法1：利用对象访问属性的方法，判断对象中是否存在key
@@ -88,9 +90,16 @@ export default {
       let keyCode = window.event ? e.keyCode : e.which;
       console.log("回车搜索", keyCode, e);
       if (keyCode === 13) {
-        this.getForm();
+        this.currentPage=1;
+        this.getForm(this.currentPage);
       }
     },
+    //监听翻页页码变化
+    handleCurrentChange(currentPage) {
+      this.currentPage = currentPage;        //然后将当前页 = 改变的值
+      console.log(this.currentPage)        //点击第几页
+      this.getForm(this.currentPage)
+    }
   }
 }
 </script>
@@ -98,6 +107,7 @@ export default {
 <style scoped>
 .el-row {
   margin-bottom: 20px;
+  min-height: 800px;
 }
 .el-col {
   border-radius: 4px;
