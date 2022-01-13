601f1e4eec14fd7568a6df10f306a08720c7539e
T480Mac <942385056@qq.com>
2022-01-13 09:48:33 +0800

    新增收藏和稍后再看功能

--- src/components/getData.vue
+++ src/components/getData.vue
@@ -1,15 +1,15 @@
 <template>
   <el-row>
-    <el-col  :span="5">
-        <img src="../assets/logo.png">
+    <el-col :span="5">
+      <img src="../assets/logo.png">
       <br/>
       <el-button type="info" @click="beautifulDate('1')">测试函数</el-button>
       <br/>
-      <el-button  @click="showCollect">收藏</el-button>
+      <el-button @click="showCollect">收藏</el-button>
       <br/>
-      <el-button  @click="showLookLater">稍后再看</el-button>
+      <el-button @click="showLookLater">稍后再看</el-button>
     </el-col>
-    <el-col  :span="10">
+    <el-col :span="10">
       <div style="margin-top: 15px">
         <el-input
             v-model="title"
@@ -23,131 +23,127 @@
         </el-input>
       </div>
       <el-table :data="tableData" style="width: 100%" @row-click="openurl">
-        <el-table-column  width="180">
+        <el-table-column width="250">
           <template #default="scope">
-            <p>{{scope.row.title}}</p>
-            <p>{{getLocalTime(scope.row.ctime)}}</p>
+            <p>{{ scope.row.title }}</p>
+            <p>{{ getLocalTime(scope.row.ctime) }}</p>
           </template>
         </el-table-column>
         <el-table-column align="right">
           <template #default="scope">
             <el-button size="small" @click="collect(scope.row)"
-            >收藏</el-button
+            >收藏
+            </el-button
             >
             <el-button
                 size="small"
-                type="danger"
                 @click="lookLater(scope.row)"
-            >稍后再看</el-button
-            >
+            >稍后再看
+            </el-button>
+            <el-button
+                size="small"
+                @click="deleteFromDb(scope.row.id)"
+            >我要删了
+            </el-button>
+
           </template>
         </el-table-column>
         <el-table-column align="right">
-        <template #default="scope">
-          <el-image
-              style="width: 200px; height: 100px"
-              :src="scope.row.pic"
-              :fit="contain"
-          ></el-image>
-        </template>
+          <template #default="scope">
+            <el-image
+                style="width: 200px; height: 100px"
+                :src="scope.row.pic"
+                :fit="contain"
+            ></el-image>
+          </template>
         </el-table-column>
       </el-table>
-      <el-pagination layout="total, prev, pager, next, jumper" :total="60" :page-size="10" @current-change="handleCurrentChange"></el-pagination>
+      <!--      <el-pagination layout="total, prev, pager, next, jumper" :total="60" :page-size="10" @current-change="handleCurrentChange"></el-pagination>-->
     </el-col>
     <el-col :offset="1" :span="7">
       <iframe :src=this.drawerUrl style="width: 100%;height: 100%"></iframe>
     </el-col>
   </el-row>
-<!--  <el-drawer v-model="drawer" :modal="false" :show-close="false" :open-delay="200">-->
-<!--    <iframe :src=this.drawerUrl style="width: 100%;height: 100%"></iframe>-->
-<!--  </el-drawer>-->
+  <!--  <el-drawer v-model="drawer" :modal="false" :show-close="false" :open-delay="200">-->
+  <!--    <iframe :src=this.drawerUrl style="width: 100%;height: 100%"></iframe>-->
+  <!--  </el-drawer>-->
 </template>
 
 <script>
-import {timestampToTime} from '../utils/dateUtil.js'
 const axios = require('axios');
 export default {
   name: "getData",
   data() {
     return {
-      url_favorite:'/api?m=App&c=Article&a=getHotMainArticle&uid=1638493790&page=1&pagesize=10',
-      url_java:'/api?m=App&c=index&a=getCategoryArticle&uid=1638493790&fid=8&page=1&pagesize=10',
-      url_custom:'/api?m=App&c=Article&a=searchArticle&uid=1638493790&pagesize=20&title=',
-      drawer:'',
-      title:'',
-      drawerUrl:'',
-      tableData:[],
-      currentPage:1,
-      requestBody:{title:'',pic:'',url:'',status:'',createTime:'',updateTime:''},
-      status1:"1",//收藏
-      status2:"2"//稍后再看
+      url_favorite: '/api?m=App&c=Article&a=getHotMainArticle&uid=1638493790&page=1&pagesize=6',
+      url_custom: '/api?m=App&c=Article&a=searchArticle&uid=1638493790&page=1&pagesize=20&title=',
+      drawer: '',
+      title: '',
+      drawerUrl: '',
+      tableData: [],
+      currentPage: 1,
+      requestBody: {title: '', pic: '', url: '', status: '', createTime: '', updateTime: ''},
+      status1: "1",//收藏
+      status2: "2"//稍后再看
     }
   },
   created() {
-    this.getForm('1')
+    this.getForm()
     // this.getApiList()
   },
-  methods:{
-    getForm(pageno) {
-      axios.get(this.url_custom+this.title+'&page='+pageno)
-          .then(res=> {
+  methods: {
+    getForm() {
+      axios.get(this.url_favorite)
+          .then(res => {
             let tempdata = res.data.data;
             //  方法1：利用对象访问属性的方法，判断对象中是否存在key
-                let result = [];
-                let obj = {};
-                for(let i =0; i<tempdata.length; i++){
-                     if(!obj[tempdata[i].aid]){
-                          //排除非奇点日报里面的url
-                          if(tempdata[i].url.indexOf("qidianlife")!==-1){
-                            result.push(tempdata[i]);
-                          }
-                          obj[tempdata[i].aid] = true;
-                      }
-                 }
-                console.log(result);
-                this.tableData=result;
-                this.drawerUrl=this.tableData[0].url
+            let result = [];
+            let obj = {};
+            for (let i = 0; i < tempdata.length; i++) {
+              if (!obj[tempdata[i].aid]) {
+                //排除非奇点日报里面的url
+                if (tempdata[i].url.indexOf("qidianlife") !== -1) {
+                  result.push(tempdata[i]);
+                }
+                obj[tempdata[i].aid] = true;
+              }
+            }
+            console.log(result);
+            this.tableData = result;
+            this.drawerUrl = this.tableData[0].url
           })
-          .catch(err=> { console.log(err); });
+          .catch(err => {
+            console.log(err);
+          });
     },
     openurl(row) {
       console.log(row.url);
-      this.drawer=true
-      this.drawerUrl=row.url;
+      this.drawer = true
+      this.drawerUrl = row.url;
     },
     //点击回车，进行搜索
-    searchEnterFun (e){
+    searchEnterFun(e) {
       let keyCode = window.event ? e.keyCode : e.which;
       console.log("回车搜索", keyCode, e);
       if (keyCode === 13) {
-        this.currentPage=1;
-        this.getForm(this.currentPage);
+        this.currentPage = 1;
+        this.getForm();
       }
     },
     //监听翻页页码变化
     handleCurrentChange(currentPage) {
       this.currentPage = currentPage;        //然后将当前页 = 改变的值
       console.log(this.currentPage)        //点击第几页
-      this.getForm(this.currentPage)
-    },
-    getApiList() {
-      axios.post('/article/add',{title:'hello',pic:'woc',url:'goog',status:'1',createTime:new Date().getTime(),updateTime:new Date().getTime()})
-          .then(function (response) {
-            console.log(new Date().getTime())
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+      this.getForm()
     },
     collect(row) {
-      this.requestBody.title=row.title;
-      this.requestBody.pic=row.pic;
-      this.requestBody.url=row.url;
-      this.requestBody.status=1;
-      this.requestBody.createTime=new Date().getTime();
-      this.requestBody.updateTime=new Date().getTime();
-      axios.post('/article/add',this.requestBody)
+      this.requestBody.title = row.title;
+      this.requestBody.pic = row.pic;
+      this.requestBody.url = row.url;
+      this.requestBody.status = 1;
+      this.requestBody.createTime = new Date().getTime();
+      this.requestBody.updateTime = new Date().getTime();
+      axios.post('/article/add', this.requestBody)
           .then(function (response) {
             console.log(response);
           })
@@ -156,13 +152,13 @@ export default {
           });
     },
     lookLater(row) {
-      this.requestBody.title=row.title;
-      this.requestBody.pic=row.pic;
-      this.requestBody.url=row.url;
-      this.requestBody.status=2;
-      this.requestBody.createTime=new Date().getTime();
-      this.requestBody.updateTime=new Date().getTime();
-      axios.post('/article/add',this.requestBody)
+      this.requestBody.title = row.title;
+      this.requestBody.pic = row.pic;
+      this.requestBody.url = row.url;
+      this.requestBody.status = 2;
+      this.requestBody.createTime = new Date().getTime();
+      this.requestBody.updateTime = new Date().getTime();
+      axios.post('/article/add', this.requestBody)
           .then(function (response) {
             console.log(response);
           })
@@ -171,22 +167,32 @@ export default {
           });
     },
     getLocalTime(nS) {
-      return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
-},
+      return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+    },
     showCollect() {
       axios.post('/article/list')
-          .then(res=> {
+          .then(res => {
             console.log(res)
-            this.tableData=res.data.data.list;
-            this.drawerUrl=this.tableData[0].url
+            this.tableData = res.data.data.list;
+            this.drawerUrl = this.tableData[0].url
           })
-          .catch(err=> { console.log(err); });
+          .catch(err => {
+            console.log(err);
+          });
     },
     showLookLater() {
 
+    },
+    deleteFromDb(id) {
+      axios.post('/article/delete?id=' + id)
+          .then(res => {
+            this.showCollect()
+          })
+          .catch(err => {
+            console.log(err);
+          });
     }
-
-}
+  }
 }
 </script>
 
@@ -195,26 +201,33 @@ export default {
   margin-bottom: 20px;
   min-height: 800px;
 }
+
 .el-col {
   border-radius: 4px;
 }
+
 .bg-purple-dark {
   background: #99a9bf;
 }
+
 .bg-purple {
   background: #d3dce6;
 }
+
 .bg-purple-light {
   background: #e5e9f2;
 }
+
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
 }
+
 .row-bg {
   padding: 10px 0;
   background-color: #f9fafc;
 }
+
 .text {
   font-size: 14px;
 }
